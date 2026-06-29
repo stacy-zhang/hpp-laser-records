7e7ac0e5f891c4b3c2eff3f099a36ff6226006e5
stacy-zhang <thestacyzhang@gmail.com>
2026-06-29 10:59:34 -0400

    initial commit

---
+++ plot.py
@@ -0,0 +1,6 @@
+# x-axis: wavelength (µm)
+# y-axis: peak power (Watts, log scale)
+# if needed, estimate peak power as pulse energy/pulse duration
+# colors corresponding to NIR (0.7-1.0 µm), SWIR (1.0-3.0 µm), MWIR (3.0-8.0 µm), and LWIR (8.0-12.0 µm) bands
+# useful extras: color by laser tech or year, marker size by repetition rate, 
+# hover labels or point labels with name, institution, and DOI
